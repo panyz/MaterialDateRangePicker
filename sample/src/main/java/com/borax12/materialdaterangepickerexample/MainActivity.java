0f17038b017f817b44b4cac901ce8098f728645c
zjmjm404 <zhangjm404@gmail.com>
2017-03-29 17:58:02 +0800

    修改文字

--- sample/src/main/java/com/borax12/materialdaterangepickerexample/MainActivity.java
+++ sample/src/main/java/com/borax12/materialdaterangepickerexample/MainActivity.java
@@ -58,6 +58,7 @@ public class MainActivity extends AppCompatActivity implements
             }
         });
 
+
         timeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
